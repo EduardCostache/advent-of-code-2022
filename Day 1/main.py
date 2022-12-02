493405e7a32ceb1f13ad2794e389ce27a5162953
EduardCostache <edwardcostache5@gmail.com>
2022-12-02 00:19:05 +0000

    0002: python day 1 part 2

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,18 +1,44 @@
-#----------------------------------- PART 1 -----------------------------------#
-maxCalories = 0
-currentCalories = 0
-lines = ""
+filename = 'advent-of-code-2022\Day 1\input.txt'
 
 
-with open('advent-of-code-2022\Day 1\input.txt', 'r') as f:
+def openFile(filename):
+    lines = ""
+    f = open(filename, 'r')
     lines = f.readlines()
+    f.close()
+    return lines
 
-for line in lines:
-    if line == '\n':
-        currentCalories = 0
-    else:
-        currentCalories += int(line.strip())
-        if currentCalories > maxCalories:
-            maxCalories = currentCalories
 
-print(maxCalories)
+lines = openFile(filename)
+
+
+def part1(lines):
+    maxCalories = 0
+    currentCalories = 0
+    for line in lines:
+        if line == '\n':
+            currentCalories = 0
+        else:
+            currentCalories += int(line.strip())
+            if currentCalories > maxCalories:
+                maxCalories = currentCalories
+
+    print(maxCalories)
+
+
+def part2(lines):
+    list = []
+    totalCals = 0
+
+    for line in lines:
+        if line == '\n':
+            list.append(totalCals)
+            totalCals = 0
+        else:
+            totalCals += int(line.strip())
+
+    list.sort()
+    print(list[-1] + list[-2] + list[-3])
+
+
+part2(lines)
