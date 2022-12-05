d7b5757d91390533228bff8741edb1ecda1431f2
EduardCostache <edwardcostache5@gmail.com>
2022-12-05 06:15:25 +0000

    0006: python day 2 part 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -6,6 +6,13 @@ shapeToPoints = {
     'Z': 3
 }
 
+shapeDict = {
+    #[lose, win, draw]
+    'A': ['Z', 'Y', 'X'],
+    'B': ['X', 'Z', 'Y'],
+    'C': ['Y', 'X', 'Z']
+}
+
 # A | X = Rock
 # B | Y = Paper
 # C | Z = Scissor
@@ -51,8 +58,35 @@ def part1(lines):
     print(totalScore)
 
 
-def part2():
-    pass
+def part2(lines):
+    totalScore = 0
+
+    for line in lines:
+        currentScore = 0
+        enemySign = line[0]
+        roundType = line[2]
+
+        # Z : win
+        # X : lose
+        # Y : draaw
+
+        if roundType == 'Z':
+            mySign = shapeDict[enemySign][1]
+
+            currentScore += shapeToPoints[mySign] + 6
+        elif roundType == 'Y':
+            mySign = shapeDict[enemySign][2]
+
+            currentScore += shapeToPoints[mySign] + 3
+        else:
+            mySign = shapeDict[enemySign][0]
+
+            currentScore += shapeToPoints[mySign]
+
+        totalScore += currentScore
+
+    print(totalScore)
 
 
-part1(lines)
+# part1(lines)
+part2(lines)
